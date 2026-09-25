2e08e08a888afaf5760936bef499f0c3b02036db
Hanushapavuluri <phanusha0@gmail.com>
2026-09-26 00:25:32 +0530

    Add multilingual service package

---
+++ backend/app/multilingual/__init__.py
@@ -0,0 +1,8 @@
+"""Multilingual services."""
+
+from app.multilingual.bhashini import (
+    BhashiniTranslationError,
+    BhashiniTranslator,
+)
+
+__all__ = ["BhashiniTranslationError", "BhashiniTranslator"]
